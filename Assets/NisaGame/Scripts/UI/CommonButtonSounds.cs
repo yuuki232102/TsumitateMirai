d0de65c9ae608a5a59507809a25db315999171d2
unknown <232143@student.nics.ac.jp>
2025-12-09 12:22:31 +0900

    cs修正済

--- Assets/NisaGame/Scripts/UI/CommonButtonSounds.cs
+++ Assets/NisaGame/Scripts/UI/CommonButtonSounds.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CommonButtonSounds : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
