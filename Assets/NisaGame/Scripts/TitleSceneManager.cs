0db02e0ff0027e94b4287b033542a330f5a9c656
unknown <232143@student.nics.ac.jp>
2025-12-11 13:40:05 +0900

    遷移追加

---
+++ Assets/NisaGame/Scripts/TitleSceneManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;  // ←これを忘れない！
+
+public class TitleSceneManager : MonoBehaviour
+{
+    // インスペクターで設定できるようにしておく
+    [SerializeField] private string simulationSceneName = "SimulationScene";
+    // ↑ 実際のシーン名が "Simulation" なら、その名前を入れてね
+
+    // Button_Start から呼び出す関数
+    public void OnClickStart()
+    {
+        // シミュレーション（STEP1/STEP2の画面）へ遷移
+        SceneManager.LoadScene(simulationSceneName);
+    }
+}
