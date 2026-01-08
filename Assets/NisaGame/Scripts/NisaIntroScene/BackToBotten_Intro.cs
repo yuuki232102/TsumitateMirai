a1324c73ca5df193ac063ed9a19887a2d8a858f0
yuuki232102 <nics_232102@nicsO365open.onmicrosoft.com>
2026-01-08 13:51:35 +0900

    nisa説明画面_2

---
+++ Assets/NisaGame/Scripts/NisaIntroScene/BackToBotten_Intro.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NisaIntroSceneManager : MonoBehaviour
+{
+    // 既存コードは省略
+
+    // 🔽 タイトル画面へ戻る
+    public void OnClickBackToTitle()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
