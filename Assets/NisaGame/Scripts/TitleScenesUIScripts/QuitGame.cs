3e385d052d2c5794eaf0835b612b3ebebc57e44f
yuuki232102 <nics_232102@nicsO365open.onmicrosoft.com>
2026-01-15 14:37:09 +0900

    デザイン修正_5

---
+++ Assets/NisaGame/Scripts/TitleScenesUIScripts/QuitGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    // ボタンから呼ぶ
+    public void Quit()
+    {
+        // ビルドしたアプリを終了
+        Application.Quit();
+
+#if UNITY_EDITOR
+        // Unityエディタ上では再生停止
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
